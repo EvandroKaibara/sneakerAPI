6798c5ab16250f8786f4efe7f7901092ca1ed126
EvandroKaibara <yujievandro@gmail.com>
2026-04-02 13:13:57 -0300

    FEAT - Sneaker class had been created

---
+++ src/main/java/com/sneakers/models/Sneaker.java
@@ -0,0 +1,19 @@
+package com.sneakers.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity @Table(name = "sneakers")
+@Getter @NoArgsConstructor @AllArgsConstructor @EqualsAndHashCode(of = "id")
+public class Sneaker {
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private Integer size;
+    private Float price;
+}
